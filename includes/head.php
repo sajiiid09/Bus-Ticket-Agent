6847e54ed62ea0357e080bf3125ea8968a068872
Sajid Mahmud <134144226+sajiiid09@users.noreply.github.com>
2025-11-14 06:32:39 +0600

    Support seat selection AJAX fallback

--- includes/head.php
+++ includes/head.php
@@ -11,13 +11,18 @@
     <meta name="theme-color" content="#079d49">
     <title>BusTicket - Online Bus Booking</title>
     
-    <!-- Bootstrap 4 -->
-    <link rel="stylesheet" href="assets/vendor/bootstrap4/css/bootstrap.css">
-    
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="assets/vendor/font-awesome-4.7.0/css/font-awesome.min.css">
-    
+    <!-- Bootstrap 4 (CDN for local demos) -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-9us8bmxxyC8E+6nGq6l5Ge0XEt3G5UpiRaY1oCbcnZ6+QcmGeXjrP9K/Y06p+4wE" crossorigin="anonymous">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
     <!-- Custom Styles -->
     <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Font Awesome Kit -->
+    <script src="https://kit.fontawesome.com/47a9672b47.js" crossorigin="anonymous"></script>
 </head>
 <body>
